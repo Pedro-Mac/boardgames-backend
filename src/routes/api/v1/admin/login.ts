6a2935193e20e121617e9c52d689cece39b61d4c
Pedro Machado <pedro.machado@five9.com>
2026-03-01 15:55:16 +0000

    refactor login route to use domain types

--- src/routes/api/v1/admin/login.ts
+++ src/routes/api/v1/admin/login.ts
@@ -1,61 +1,60 @@
-import { FastifyPluginAsync } from "fastify";
+import { HttpError } from "@fastify/sensible";
+import { FastifyPluginAsync, RouteGenericInterface } from "fastify";
+import { User } from "../../../../types/user";
+import { Session } from "../../../../types/session";
+import { LoginInput, LoginOutput } from "../../../../types/authentication";
+
+interface LoginRoute extends RouteGenericInterface {
+  Body: LoginInput;
+  Reply: LoginOutput | HttpError;
+}
 
 const login: FastifyPluginAsync = async (fastify): Promise<void> => {
-  fastify.post("/login", async (request, reply) => {
-    try {
-      const { email, password } = request.body as {
-        email: string;
-        password: string;
-      };
+  fastify.post<LoginRoute>("/login", async (request, reply) => {
+    const { email, password } = request.body;
 
-      const auth = await fastify.supabase.auth.signInWithPassword({
-        email,
-        password,
-      });
+    const auth = await fastify.supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
 
-      if (auth.error) {
-        console.error("Error signing in:", auth.error);
-        reply.status(401).send({ error: "Invalid credentials" });
-        return;
-      }
-
-      const permissions = await fastify.supabase
-        .from("user_permissions")
-        .select("permissions(name)")
-        .eq("user_id", auth.data.user.id)
-        .single();
-
-      if (permissions.error) {
-        console.error("Error fetching permissions:", permissions.error);
-        reply.status(500).send({ error: "Failed to fetch permissions" });
-        return;
-      }
-
-      if (!permissions.data) {
-        reply.status(403).send({ error: "No permissions found for user" });
-        return;
-      }
-
-      // @ts-ignore
-      if (permissions.data.permissions.name !== "backoffice_view") {
-        reply
-          .status(403)
-          .send({ error: "User does not have backoffice permissions" });
-        return;
-      }
-
-      const user = {};
-
-      // @ts-ignore
-      user.email = auth.data.user.email;
-
-      console.log("obj", permissions);
-
-      reply.send({ message: "Login successful", auth });
-    } catch (error) {
-      console.error("Error during login:", error);
-      reply.status(500).send({ error: "Failed to login" });
+    if (auth.error) {
+      console.error("Error signing in:", auth.error);
+      throw fastify.httpErrors.unauthorized("Invalid email or password");
     }
+
+    const permissions = await fastify.supabase
+      .from("user_permissions")
+      .select("permissions(name)")
+      .eq("user_id", auth.data.user.id)
+      .single();
+
+    if (permissions.error) {
+      console.error("Error fetching permissions:", permissions.error);
+      throw fastify.httpErrors.internalServerError(
+        "Failed to fetch user permissions",
+      );
+    }
+
+    if (!permissions.data) {
+      throw fastify.httpErrors.forbidden("User does not have permissions");
+    }
+
+    // @ts-ignore
+    if (permissions.data.permissions.name !== "backoffice_view") {
+      throw fastify.httpErrors.forbidden(
+        "User does not have the required permissions",
+      );
+    }
+
+    const user = {};
+
+    // @ts-ignore
+    user.email = auth.data.user.email;
+
+    console.log("obj", permissions);
+
+    reply.send({ message: "Login successful", auth });
   });
 };
 
