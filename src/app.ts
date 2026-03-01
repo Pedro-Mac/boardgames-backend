e8c25666b91535f748ee1828700bb582b70cbeef
Pedro Machado <pedro.machado@five9.com>
2026-03-01 15:17:19 +0000

    remove duplicate code from entry point file

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import { join } from "node:path";
 import AutoLoad, { AutoloadPluginOptions } from "@fastify/autoload";
 import { FastifyPluginAsync, FastifyServerOptions } from "fastify";
-import { createClient } from "@supabase/supabase-js";
 
 export interface AppOptions
   extends FastifyServerOptions, Partial<AutoloadPluginOptions> {}
@@ -12,68 +11,6 @@ const app: FastifyPluginAsync<AppOptions> = async (
   fastify,
   opts,
 ): Promise<void> => {
-  // Place here your custom code!
-  const supabase = createClient(
-    process.env.SUPABASE_URL || "",
-    process.env.SUPABASE_KEY || "",
-  );
-
-  fastify.post("/api/v1/admin/login", async (request, reply) => {
-    try {
-      const { email, password } = request.body as {
-        email: string;
-        password: string;
-      };
-
-      const auth = await supabase.auth.signInWithPassword({
-        email,
-        password,
-      });
-
-      if (auth.error) {
-        console.error("Error signing in:", auth.error);
-        reply.status(401).send({ error: "Invalid credentials" });
-        return;
-      }
-
-      const permissions = await supabase
-        .from("user_permissions")
-        .select("permissions(name)")
-        .eq("user_id", auth.data.user.id)
-        .single();
-
-      if (permissions.error) {
-        console.error("Error fetching permissions:", permissions.error);
-        reply.status(500).send({ error: "Failed to fetch permissions" });
-        return;
-      }
-
-      if (!permissions.data) {
-        reply.status(403).send({ error: "No permissions found for user" });
-        return;
-      }
-
-      // @ts-ignore
-      if (permissions.data.permissions.name !== "backoffice_view") {
-        reply
-          .status(403)
-          .send({ error: "User does not have backoffice permissions" });
-        return;
-      }
-
-      const user = {};
-
-      user.email = auth.data.user.email;
-
-      console.log("obj", permissions);
-
-      reply.send({ message: "Login successful", user });
-    } catch (error) {
-      console.error("Error during login:", error);
-      reply.status(500).send({ error: "Failed to login" });
-    }
-  });
-
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
