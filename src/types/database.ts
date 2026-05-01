57e8d0c196b253d779e870127d85528749c4fef7
Pedro Machado <pedro.machado@five9.com>
2026-05-01 17:14:02 +0100

    sync database tables

--- src/types/database.ts
+++ src/types/database.ts
@@ -4,226 +4,313 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export type Database = {
   // Allows to automatically instantiate createClient with right options
   // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
   __InternalSupabase: {
-    PostgrestVersion: "14.1";
-  };
+    PostgrestVersion: "14.1"
+  }
   public: {
     Tables: {
       categories: {
         Row: {
-          created_at: string;
-          id: string;
-          name: string;
-        };
+          created_at: string
+          id: string
+          name: string
+        }
         Insert: {
-          created_at?: string;
-          id?: string;
-          name: string;
-        };
+          created_at?: string
+          id?: string
+          name: string
+        }
         Update: {
-          created_at?: string;
-          id?: string;
-          name?: string;
-        };
-        Relationships: [];
-      };
+          created_at?: string
+          id?: string
+          name?: string
+        }
+        Relationships: []
+      }
       game_categories: {
         Row: {
-          category_id: string;
-          game_id: string;
-        };
+          category_id: string
+          game_id: string
+        }
         Insert: {
-          category_id: string;
-          game_id: string;
-        };
+          category_id: string
+          game_id: string
+        }
         Update: {
-          category_id?: string;
-          game_id?: string;
-        };
+          category_id?: string
+          game_id?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "game_categories_game_id_fkey";
-            columns: ["game_id"];
-            isOneToOne: false;
-            referencedRelation: "games";
-            referencedColumns: ["id"];
+            foreignKeyName: "game_categories_category_id_fkey"
+            columns: ["category_id"]
+            isOneToOne: false
+            referencedRelation: "categories"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "game_categories_category_id_fkey";
-            columns: ["category_id"];
-            isOneToOne: false;
-            referencedRelation: "categories";
-            referencedColumns: ["id"];
+            foreignKeyName: "game_categories_game_id_fkey"
+            columns: ["game_id"]
+            isOneToOne: false
+            referencedRelation: "games"
+            referencedColumns: ["id"]
           },
-        ];
-      };
+        ]
+      }
+      game_expansions: {
+        Row: {
+          base_game_id: string
+          expansion_id: string
+        }
+        Insert: {
+          base_game_id: string
+          expansion_id: string
+        }
+        Update: {
+          base_game_id?: string
+          expansion_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "game_expansions_base_game_id_fkey"
+            columns: ["base_game_id"]
+            isOneToOne: false
+            referencedRelation: "games"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "game_expansions_expansion_id_fkey"
+            columns: ["expansion_id"]
+            isOneToOne: false
+            referencedRelation: "games"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      game_mechanics: {
+        Row: {
+          game_id: string
+          mechanic_id: string
+        }
+        Insert: {
+          game_id: string
+          mechanic_id: string
+        }
+        Update: {
+          game_id?: string
+          mechanic_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "game_mechanics_game_id_fkey"
+            columns: ["game_id"]
+            isOneToOne: false
+            referencedRelation: "games"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "game_mechanics_mechanic_id_fkey"
+            columns: ["mechanic_id"]
+            isOneToOne: false
+            referencedRelation: "mechanics"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
       games: {
         Row: {
-          added_by: string | null;
-          age_recommendation: number;
-          created_at: string;
-          description: string;
-          id: string;
-          image_url: string | null;
-          max_play_time: number;
-          max_players: number;
-          min_play_time: number;
-          min_players: number;
-          name: string;
-          price: number;
-          publisher: string;
-          stock: number;
-          updated_by: string | null;
-          year_published: number;
-        };
+          added_by: string | null
+          artists: string[]
+          authors: string[]
+          created_at: string
+          description: string
+          designers: string[]
+          game_type: string
+          id: string
+          illustrators: string[]
+          image_url: string | null
+          max_play_time: number
+          max_players: number
+          min_age: number
+          min_play_time: number
+          min_players: number
+          price: number
+          publisher: string
+          stock: number
+          title: string
+          updated_by: string | null
+          year_published: number
+        }
         Insert: {
-          added_by?: string | null;
-          age_recommendation: number;
-          created_at?: string;
-          description: string;
-          id?: string;
-          image_url?: string | null;
-          max_play_time: number;
-          max_players: number;
-          min_play_time: number;
-          min_players: number;
-          name: string;
-          price: number;
-          publisher: string;
-          stock?: number;
-          updated_by?: string | null;
-          year_published: number;
-        };
+          added_by?: string | null
+          artists?: string[]
+          authors?: string[]
+          created_at?: string
+          description: string
+          designers?: string[]
+          game_type?: string
+          id?: string
+          illustrators?: string[]
+          image_url?: string | null
+          max_play_time: number
+          max_players: number
+          min_age: number
+          min_play_time: number
+          min_players: number
+          price: number
+          publisher: string
+          stock?: number
+          title: string
+          updated_by?: string | null
+          year_published: number
+        }
         Update: {
-          added_by?: string | null;
-          age_recommendation?: number;
-          created_at?: string;
-          description?: string;
-          id?: string;
-          image_url?: string | null;
-          max_play_time?: number;
-          max_players?: number;
-          min_play_time?: number;
-          min_players?: number;
-          name?: string;
-          price?: number;
-          publisher?: string;
-          stock?: number;
-          updated_by?: string | null;
-          year_published?: number;
-        };
-        Relationships: [];
-      };
+          added_by?: string | null
+          artists?: string[]
+          authors?: string[]
+          created_at?: string
+          description?: string
+          designers?: string[]
+          game_type?: string
+          id?: string
+          illustrators?: string[]
+          image_url?: string | null
+          max_play_time?: number
+          max_players?: number
+          min_age?: number
+          min_play_time?: number
+          min_players?: number
+          price?: number
+          publisher?: string
+          stock?: number
+          title?: string
+          updated_by?: string | null
+          year_published?: number
+        }
+        Relationships: []
+      }
+      mechanics: {
+        Row: {
+          created_at: string
+          id: string
+          name: string
+        }
+        Insert: {
+          created_at?: string
+          id?: string
+          name: string
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          name?: string
+        }
+        Relationships: []
+      }
       permissions: {
         Row: {
-          id: string;
-          name: string;
-        };
+          id: string
+          name: string
+        }
         Insert: {
-          id?: string;
-          name: string;
-        };
+          id?: string
+          name: string
+        }
         Update: {
-          id?: string;
-          name?: string;
-        };
-        Relationships: [];
-      };
+          id?: string
+          name?: string
+        }
+        Relationships: []
+      }
       user_permissions: {
         Row: {
-          permission_id: string;
-          user_id: string;
-        };
+          permission_id: string
+          user_id: string
+        }
         Insert: {
-          permission_id: string;
-          user_id: string;
-        };
+          permission_id: string
+          user_id: string
+        }
         Update: {
-          permission_id?: string;
-          user_id?: string;
-        };
+          permission_id?: string
+          user_id?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "user_permissions_permission_id_fkey";
-            columns: ["permission_id"];
-            isOneToOne: false;
-            referencedRelation: "permissions";
-            referencedColumns: ["id"];
+            foreignKeyName: "user_permissions_permission_id_fkey"
+            columns: ["permission_id"]
+            isOneToOne: false
+            referencedRelation: "permissions"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "user_permissions_user_id_fkey";
-            columns: ["user_id"];
-            isOneToOne: false;
-            referencedRelation: "users";
-            referencedColumns: ["id"];
+            foreignKeyName: "user_permissions_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
           },
-        ];
-      };
+        ]
+      }
       users: {
         Row: {
-          email: string | null;
-          id: string;
-          name: string | null;
-        };
+          email: string | null
+          id: string
+          name: string | null
+        }
         Insert: {
-          email?: string | null;
-          id: string;
-          name?: string | null;
-        };
+          email?: string | null
+          id: string
+          name?: string | null
+        }
         Update: {
-          email?: string | null;
-          id?: string;
-          name?: string | null;
-        };
-        Relationships: [];
-      };
-    };
+          email?: string | null
+          id?: string
+          name?: string | null
+        }
+        Relationships: []
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      set_user_claims: {
-        Args: { uid: string };
-        Returns: undefined;
-      };
-    };
+      set_user_claims: { Args: { uid: string }; Returns: undefined }
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
-};
+      [_ in never]: never
+    }
+  }
+}
 
-type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">;
+type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">
 
-type DefaultSchema = DatabaseWithoutInternals[Extract<
-  keyof Database,
-  "public"
->];
+type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">]
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
     | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
         DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
       DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R;
+      Row: infer R
     }
     ? R
     : never
@@ -231,98 +318,98 @@ export type Tables<
         DefaultSchema["Views"])
     ? (DefaultSchema["Tables"] &
         DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
-        Row: infer R;
+        Row: infer R
       }
       ? R
       : never
-    : never;
+    : never
 
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I;
+      Insert: infer I
     }
     ? I
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
     ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Insert: infer I;
+        Insert: infer I
       }
       ? I
       : never
-    : never;
+    : never
 
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U;
+      Update: infer U
     }
     ? U
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
     ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Update: infer U;
+        Update: infer U
       }
       ? U
       : never
-    : never;
+    : never
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
     | keyof DefaultSchema["Enums"]
     | { schema: keyof DatabaseWithoutInternals },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
 > = DefaultSchemaEnumNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
     ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-    : never;
+    : never
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof DefaultSchema["CompositeTypes"]
     | { schema: keyof DatabaseWithoutInternals },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
 > = PublicCompositeTypeNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
     ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never;
+    : never
 
 export const Constants = {
   public: {
     Enums: {},
   },
-} as const;
+} as const
